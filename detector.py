cef862a493a7253ffa81a04e1ac6954cb19e52d5
Isaiah <124836428+w1ggle@users.noreply.github.com>
2023-09-30 19:46:24 -0400

    newest model

--- detector.py
+++ detector.py
@@ -7,4 +7,4 @@ model = YOLO("best.pt")
 #png.save()
 #print(png)
 
-results = model(source = 0, show = True, max_det = 1, save = False, conf = 0.5) #source = 0 means webcam, show what the ai is doing (need this on for webcam), max_det is number of boxes, save is save the detection outputs, conf is minimum conf to show a box
+results = model(source = 0, show = True, max_det = 1, conf = 0.5, save = False, verbose=False) #source = 0 means webcam, show what the ai is doing (need this on for webcam), max_det is number of boxes, save is save the detection outputs, conf is minimum conf to show a box
