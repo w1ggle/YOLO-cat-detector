009bd7f8c2807c1b88300067aae3643fb152c39e
Isaiah P <124836428+w1ggle@users.noreply.github.com>
2023-10-02 13:38:41 -0400

    using webcam

--- detector.py
+++ detector.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 #from heic2png import HEIC2PNG
+#import cv2
 
 model = YOLO("best.pt")
 
@@ -7,4 +8,12 @@ model = YOLO("best.pt")
 #png.save()
 #print(png)
 
-results = model(source = 0, show = True, max_det = 1, conf = 0.1, save = False, verbose=False) #source = 0 means webcam, show what the ai is doing (need this on for webcam), max_det is number of boxes, save is save the detection outputs, conf is minimum conf to show a box
+#for i in range(0,100):
+#    cap = cv2.VideoCapture(i)
+#    test = cap.read()
+#    print(i)
+#    print(test)
+    
+
+results = model(source = 0, show = True, max_det = 1, conf = 0.1, save = False, verbose=True) #source = 0 means webcam, show what the ai is doing (need this on for webcam), max_det is number of boxes, save is save the detection outputs, conf is minimum conf to show a box
+    
